f8f5e3f10c25610e85d798963afe0841e42bf4ea
abhishekch405 <abhishekch405@gmail.com>
2022-10-30 11:47:59 +0530

    reading and writing file

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 const http = require('http');
-const { url } = require('inspector');
 
-function rqlistener(req, res) {
-    //console.log('Abhishek');
-    //console.log(req);
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
-    //console.log('Abhishek');
+const routes = require('./routes')
 
-    const urll = req.url;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    if (urll.match('home')) {
-        res.write('<body><h1>Welcome Home</h1></body>');
-    } else if (urll.match('about')) {
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-    } else {
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-    }
-    res.write('</html>');
-    res.end();
-}
-
-const server = http.createServer(rqlistener);
+const server = http.createServer(routes);
 server.listen(3000);
