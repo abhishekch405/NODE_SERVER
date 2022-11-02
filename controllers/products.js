eb104ff73d9243a0675b20d671dd98ef29512d91
abhishekch405 <abhishekch405@gmail.com>
2022-11-03 00:35:33 +0530

    Added Models in the Project

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,18 @@
-const path = require('path')
+const path = require('path');
+const Product = require('../models/product');
+
+//const arr=[];
 exports.getAddProducts = (req, res, next) => {
     console.log("In admin file!");
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 }
 
 exports.getPostProducts = (req, res, next) => {
-    console.log(req.body);
+    //console.log('This is req.body', req.body);
+    const newprod = new Product(req.body.title);
+    console.log(newprod);
+    newprod.save();
+    console.log("Product Fetchall", Product.fetchall((products) => { console.log(products) }));
     res.redirect('/');
 }
 
